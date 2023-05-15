ac5c3f3f726652638a41ea4adc2bb59dfbef7c5b
Hoya Kim <wbstory@storymate.net>
2023-05-16 05:44:05 +0900

    Added somple docs

--- src/routes/drawing-app/+page.svelte
+++ src/routes/drawing-app/+page.svelte
@@ -39,6 +39,23 @@
   function clearCanvas() {
     ctx.clearRect(0, 0, width, height);
   }
+
+  // Svelte Event modifiers
+  // https://svelte.dev/tutorial/event-modifiers
+  /*
+    The full list of modifiers:
+
+    preventDefault — calls event.preventDefault() before running the handler. Useful for client-side form handling, for example.
+    stopPropagation — calls event.stopPropagation(), preventing the event reaching the next element
+    passive — improves scrolling performance on touch/wheel events (Svelte will add it automatically where it's safe to do so)
+    nonpassive — explicitly set passive: false
+    capture — fires the handler during the capture phase instead of the bubbling phase (MDN docs)
+    once — remove the handler after the first time it runs
+    self — only trigger handler if event.target is the element itself
+    trusted — only trigger handler if event.isTrusted is true. I.e. if the event is triggered by a user action.
+
+    You can chain modifiers together, e.g. on:click|once|capture={...}.
+  */
 </script>
 
 <svelte:head>
